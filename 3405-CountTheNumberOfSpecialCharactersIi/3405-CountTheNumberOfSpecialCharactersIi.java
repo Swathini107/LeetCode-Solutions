2255a9fe7f9ecfc76f45fc1792617c14eab72f22
Swathini Balachandar <151824773+Swathini107@users.noreply.github.com>
2026-07-07 22:58:57 +0530

    Create 3405-CountTheNumberOfSpecialCharactersIi

---
+++ 3405-CountTheNumberOfSpecialCharactersIi/3405-CountTheNumberOfSpecialCharactersIi.java
@@ -0,0 +1,38 @@
+// Last updated: 7/7/2026, 10:58:56 PM
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int[] lastLower = new int[26];
+        int[] firstUpper = new int[26];
+
+        // initialize
+        for (int i = 0; i < 26; i++) {
+            lastLower[i] = -1;
+            firstUpper[i] = Integer.MAX_VALUE;
+        }
+
+        // store positions
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+
+            if (Character.isLowerCase(ch)) {
+                lastLower[ch - 'a'] = i;
+            } else {
+                firstUpper[ch - 'A'] = Math.min(firstUpper[ch - 'A'], i);
+            }
+        }
+
+        int count = 0;
+
+        // check condition
+        for (int i = 0; i < 26; i++) {
+            if (lastLower[i] != -1 &&
+                firstUpper[i] != Integer.MAX_VALUE &&
+                lastLower[i] < firstUpper[i]) {
+
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
