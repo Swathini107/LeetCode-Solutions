a354cfffbc2df0d59b96d55f4264e658f61ee505
Swathini Balachandar <151824773+Swathini107@users.noreply.github.com>
2026-07-07 22:59:28 +0530

    Create 2554-MinimumTotalDistanceTraveled

---
+++ 2554-MinimumTotalDistanceTraveled/2554-MinimumTotalDistanceTraveled.java
@@ -0,0 +1,47 @@
+// Last updated: 7/7/2026, 10:59:28 PM
+import java.util.*;
+
+class Solution {
+    public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
+        
+        Collections.sort(robot);
+        Arrays.sort(factory, (a, b) -> a[0] - b[0]);
+        
+        // Expand factories
+        List<Integer> slots = new ArrayList<>();
+        for (int[] f : factory) {
+            int pos = f[0], limit = f[1];
+            for (int i = 0; i < limit; i++) {
+                slots.add(pos);
+            }
+        }
+        
+        int n = robot.size();
+        int m = slots.size();
+        
+        long[][] dp = new long[n + 1][m + 1];
+        
+        for (long[] row : dp) Arrays.fill(row, Long.MAX_VALUE);
+        
+        // Base case
+        for (int j = 0; j <= m; j++) dp[0][j] = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                
+                // Skip slot
+                dp[i][j] = dp[i][j - 1];
+                
+                // Use slot
+                if (dp[i - 1][j - 1] != Long.MAX_VALUE) {
+                    long cost = dp[i - 1][j - 1] +
+                        Math.abs(robot.get(i - 1) - slots.get(j - 1));
+                    
+                    dp[i][j] = Math.min(dp[i][j], cost);
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+}
