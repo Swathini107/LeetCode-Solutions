8724cce304766fead2d2adc7f7d41f3a4019c036
Swathini Balachandar <151824773+Swathini107@users.noreply.github.com>
2026-07-07 22:59:29 +0530

    Create 2550-WordsWithinTwoEditsOfDictionary

---
+++ 2550-WordsWithinTwoEditsOfDictionary/2550-WordsWithinTwoEditsOfDictionary.java
@@ -0,0 +1,29 @@
+// Last updated: 7/7/2026, 10:59:29 PM
+import java.util.*;
+
+class Solution {
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        List<String> result = new ArrayList<>();
+        
+        for (String q : queries) {
+            for (String d : dictionary) {
+                
+                int diff = 0;
+                
+                for (int i = 0; i < q.length(); i++) {
+                    if (q.charAt(i) != d.charAt(i)) {
+                        diff++;
+                        if (diff > 2) break; // early stop
+                    }
+                }
+                
+                if (diff <= 2) {
+                    result.add(q);
+                    break; // no need to check other dictionary words
+                }
+            }
+        }
+        
+        return result;
+    }
+}
