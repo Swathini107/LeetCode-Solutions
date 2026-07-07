9117ec7389e98b9beab5038c09c3c79f2dad51e8
Swathini Balachandar <151824773+Swathini107@users.noreply.github.com>
2026-07-07 23:01:00 +0530

    Create 1054-ComplementOfBase10Integer

---
+++ 1054-ComplementOfBase10Integer/1054-ComplementOfBase10Integer.java
@@ -0,0 +1,17 @@
+// Last updated: 7/7/2026, 11:00:59 PM
+class Solution {
+    public int bitwiseComplement(int n) {
+        
+        if (n == 0) return 1;
+
+        int mask = 0;
+        int temp = n;
+
+        while (temp > 0) {
+            mask = (mask << 1) | 1;
+            temp >>= 1;
+        }
+
+        return mask ^ n;
+    }
+}
