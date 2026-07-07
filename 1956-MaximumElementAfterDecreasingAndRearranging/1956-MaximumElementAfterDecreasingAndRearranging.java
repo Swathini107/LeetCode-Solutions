7f2a2c0a491b87a2b32740165a367218b508e66b
Swathini Balachandar <151824773+Swathini107@users.noreply.github.com>
2026-07-07 23:00:02 +0530

    Create 1956-MaximumElementAfterDecreasingAndRearranging

---
+++ 1956-MaximumElementAfterDecreasingAndRearranging/1956-MaximumElementAfterDecreasingAndRearranging.java
@@ -0,0 +1,14 @@
+// Last updated: 7/7/2026, 11:00:02 PM
+class Solution {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+
+        arr[0] = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = Math.min(arr[i], arr[i - 1] + 1);
+        }
+
+        return arr[arr.length - 1];
+    }
+}
