c0e7bcc3ca4026f93843363d5c46e0eb0f4082e9
Swathini Balachandar <151824773+Swathini107@users.noreply.github.com>
2026-07-07 22:59:08 +0530

    Create 2999-CheckIfStringsCanBeMadeEqualWithOperationsI

---
+++ 2999-CheckIfStringsCanBeMadeEqualWithOperationsI/2999-CheckIfStringsCanBeMadeEqualWithOperationsI.java
@@ -0,0 +1,18 @@
+// Last updated: 7/7/2026, 10:59:08 PM
+class Solution {
+    public boolean canBeEqual(String s1, String s2) {
+        char[] a1 = new char[]{s1.charAt(0), s1.charAt(2)};
+        char[] a2 = new char[]{s2.charAt(0), s2.charAt(2)};
+        
+        char[] b1 = new char[]{s1.charAt(1), s1.charAt(3)};
+        char[] b2 = new char[]{s2.charAt(1), s2.charAt(3)};
+        
+        java.util.Arrays.sort(a1);
+        java.util.Arrays.sort(a2);
+        java.util.Arrays.sort(b1);
+        java.util.Arrays.sort(b2);
+        
+        return java.util.Arrays.equals(a1, a2) &&
+               java.util.Arrays.equals(b1, b2);
+    }
+}
