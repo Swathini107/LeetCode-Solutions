0266bb81a5d1d020c9fc2f78fcf2ee499b90cbeb
Swathini Balachandar <151824773+Swathini107@users.noreply.github.com>
2026-07-18 10:13:27 +0530

    Create 2106-FindGreatestCommonDivisorOfArray

---
+++ 2106-FindGreatestCommonDivisorOfArray/2106-FindGreatestCommonDivisorOfArray.java
@@ -0,0 +1,22 @@
+// Last updated: 7/18/2026, 10:13:27 AM
+class Solution {
+    public int findGCD(int[] nums) {
+        int min = nums[0], max = nums[0];
+
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        return gcd(min, max);
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
