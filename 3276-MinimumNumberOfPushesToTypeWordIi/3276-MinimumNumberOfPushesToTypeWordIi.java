8812ef69285b7e4973468bdc6f7d886845f71b94
Swathini Balachandar <151824773+Swathini107@users.noreply.github.com>
2026-07-31 18:07:07 +0530

    Create 3276-MinimumNumberOfPushesToTypeWordIi

---
+++ 3276-MinimumNumberOfPushesToTypeWordIi/3276-MinimumNumberOfPushesToTypeWordIi.java
@@ -0,0 +1,24 @@
+// Last updated: 7/31/2026, 6:07:06 PM
+class Solution {
+    public int minimumPushes(String word) {
+
+        //Calculate frequency of each character
+        int[] freq = new int[26];
+
+        for(char ch: word.toCharArray())
+        {
+            freq[ch - 'a']++;
+        }
+
+        Arrays.sort(freq);
+
+        int ans=0;
+        for(int i=25,pos=0; i>=0 && freq[i] > 0; i--,pos++)
+        {
+            ans += freq[i]*(pos/8 + 1);
+        }
+        return ans;
+
+        
+    }
+}
