7f3f56cd64aa4bee567337996d747c90774a8d68
Swathini Balachandar <151824773+Swathini107@users.noreply.github.com>
2026-08-24 11:00:49 +0530

    Create 3918-CheckDivisibilityByDigitSumAndProduct

---
+++ 3918-CheckDivisibilityByDigitSumAndProduct/3918-CheckDivisibilityByDigitSumAndProduct.java
@@ -0,0 +1,15 @@
+// Last updated: 8/24/2026, 11:00:49 AM
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int sum = 0, product = 1, x = n;
+
+        while (x > 0) {
+            int d = x % 10;
+            sum += d;
+            product *= d;
+            x /= 10;
+        }
+
+        return n % (sum + product) == 0;
+    }
+}
