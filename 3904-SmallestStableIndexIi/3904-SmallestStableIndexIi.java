8d7835e50889ba381ec771e0aab5415e8f563a8a
Swathini Balachandar <151824773+Swathini107@users.noreply.github.com>
2026-09-08 21:40:44 +0530

    Create 3904-SmallestStableIndexIi

---
+++ 3904-SmallestStableIndexIi/3904-SmallestStableIndexIi.java
@@ -0,0 +1,19 @@
+// Last updated: 9/8/2026, 9:40:43 PM
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int maxSoFar = -1;
+        int cand = 0, max = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            maxSoFar = Math.max(maxSoFar, nums[i]);
+
+            if (cand == i)
+                max = maxSoFar;
+
+            if (nums[i] < max - k)
+                cand = i + 1;
+        }
+
+        return cand < nums.length ? cand : -1;
+    }
+}
